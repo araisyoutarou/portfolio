55f44b6c9ccddadb5ddb8f0e7c52eef472bfe709
arai <syotaroara@gmail.com>
2021-12-16 12:59:33 +0000

    12月16日

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', 'BooksController@show')->name('top_page');
+    Route::get('/', 'CalendarController@show');
+});
+
